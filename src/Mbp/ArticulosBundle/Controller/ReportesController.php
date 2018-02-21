800d7bc742777c91b8a0d3f9c7efb9185f986139
Laureano <laureano.panzitta@gmail.com>
2018-02-21 15:06:39 -0300

    Reporte de historico de mov de art casi terminado

--- src/Mbp/ArticulosBundle/Controller/ReportesController.php
+++ src/Mbp/ArticulosBundle/Controller/ReportesController.php
@@ -2,6 +2,7 @@
 namespace Mbp\ArticulosBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,12 +10,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Mbp\ArticulosBundle\Entity\FormulasRepository;
 use Mbp\ArticulosBundle\Entity\Formulas;
 
+
 class ReportesController extends Controller
 {	
-	/*
-	 * RECIBE EL ID DE UNA FORMULA Y GENERA REPORTE DE LA ESTRUCTURA
-	 * */
-	 
+	 /**
+     * @Route("/extranet/generaReporteEstructura", name="mbp_formulas_generaReporte", options={"expose"=true})
+     */
 	public function generaReporteEstructuraAction()
 	{
 		$repo = $this->get('reporteador');		
@@ -128,6 +129,9 @@ class ReportesController extends Controller
 		);
 	}
 	
+	/**
+     * @Route("/extranet/muestraReporteEstructura", name="mbp_formulas_muestraReporte", options={"expose"=true})
+     */
 	public function muestraReporteEstructuraAction()
 	{
 		$kernel = $this->get('kernel');	
@@ -144,4 +148,107 @@ class ReportesController extends Controller
 
         return $response;
 	}
+	
+	
+	 /**
+     * @Route("/reporteHistoricoMov", name="mbp_reportes_historicoMov", options={"expose"=true})
+     */
+	public function reporteHistoricoMov()
+	{
+		$repo = $this->get('reporteador');		
+		$kernel = $this->get('kernel');	
+		
+		/*
+		 * Recibo parametros del request 
+		 */
+		$em = $this->getDoctrine()->getManager();
+		$req = $this->getRequest();
+		$idNodo = (int)$req->request->get('idNodo');
+		$tipoCambio = $this->get('TipoCambio');
+		$tc = (float)$tipoCambio->getTipoCambio();
+		
+		$jru = $repo->jru();
+				
+		$ruta = $kernel->locateResource('@MbpArticulosBundle/Reportes/EstructuraDeMateriales.jrxml');
+		$rutaLogo = $repo->getRutaLogo($kernel);
+		
+		//Ruta de destino
+		$destino = $kernel->locateResource('@MbpArticulosBundle/Resources/public/pdf/').'Estructura_materiales.pdf';
+		
+		//Parametros HashMap
+		$param = $repo->getJava('java.util.HashMap');
+		$param->put('idNodo', $idNodo);
+		$param->put('rutaLogo', $rutaLogo);
+		$param->put('tc', $tc);
+				
+		$conn = $repo->getJdbc();
+		
+		/*
+		 * SQL
+		 * 
+		 */
+		$sql = "
+			SELECT
+				RemitosClientes.fecha AS fechaRemito,
+				RemitosClientes.remitoNum AS remitoNum,
+				RemitosClientesDetalles.cantidad AS remitoCant,
+				RemitosClientesDetalles.descripcion AS detalleRem,
+				ProveedorRem.rsocial AS proveedorRem,
+				ClienteRem.rsocial AS clienteRem,
+			
+				MovimientosArticulos.fechaMovimiento,
+				MovimientosArticulos.comprobanteNum AS movimientoNum,
+				MovimientosArticulos.tipoMovimiento,
+				ConceptosStock.concepto AS conceptoMovimiento,
+				ProveedorMov.rsocial AS proveedorMov,
+				ClienteMov.rsocial AS clienteMov,
+				DetalleMovArt.cantidad AS cantMov,
+				DetalleMovArt.descripcion AS descripcionMov,
+			
+				articulos.codigo,
+				articulos.descripcion AS descArt
+			FROM (SELECT
+			     DetalleMovArt.`id` AS MOVIMIENTO_REMITO_ID,
+			     DetalleMovArt.`descripcion` AS DESCRIPCION_MOV,
+			     DetalleMovArt.`articuloId` AS ARTICULO_ID
+			FROM
+			     `DetalleMovArt` DetalleMovArt
+			UNION
+			SELECT
+			     RemitosClientesDetalles.`id` AS RemitosClientesDetalles_id,
+			     RemitosClientesDetalles.`descripcion` AS RemitosClientesDetalles_descripcion,
+			     RemitosClientesDetalles.`articuloId` AS RemitosClientesDetalles_articuloId
+			FROM
+			     `RemitosClientesDetalles` RemitosClientesDetalles) AS SUB
+			LEFT JOIN movimientos_detalles ON MOVIMIENTO_REMITO_ID = movimientos_detalles.detallemovart_id
+			LEFT JOIN MovimientosArticulos ON movimientos_detalles.movimientosarticulos_id = MovimientosArticulos.id
+			LEFT JOIN Proveedor AS ProveedorMov ON ProveedorMov.id = MovimientosArticulos.proveedorId
+			LEFT JOIN cliente AS ClienteMov ON ClienteMov.idCliente = MovimientosArticulos.clienteId
+			LEFT JOIN ConceptosStock ON ConceptosStock.id = MovimientosArticulos.conceptoId
+			LEFT JOIN DetalleMovArt ON DetalleMovArt.id = movimientos_detalles.detallemovart_id
+			
+			LEFT JOIN RemitoClientes_detalle ON RemitoClientes_detalle.remitosclientes_id = MOVIMIENTO_REMITO_ID
+			LEFT JOIN RemitosClientes ON RemitosClientes.id = RemitoClientes_detalle.remitosclientes_id
+			LEFT JOIN RemitosClientesDetalles ON RemitosClientesDetalles.id = RemitoClientes_detalle.remitosclientesdetalles_id
+			LEFT JOIN Proveedor AS ProveedorRem ON ProveedorRem.id = RemitosClientes.proveedorId
+			LEFT JOIN cliente AS ClienteRem ON ClienteRem.idCliente = RemitosClientes.clienteId
+			
+			LEFT JOIN articulos ON articulos.idArticulos = ARTICULO_ID
+		";
+		 /*
+		  * FIN SQL
+		  */
+		
+		//Exportamos el reporte
+		$jru->runPdfFromSql($ruta, $destino, $param,$sql,$conn->getConnection());
+		
+		
+		return new Response(
+			json_encode(array(
+				'success' => true,
+				'reporte' => $destino,	
+			))
+		);
+	}
+	
 }
