a7b78653644bfeb822e5d832a21dfa29870e9b4c
Laureano <laureano.panzitta@gmail.com>
2018-06-19 13:19:15 -0300

    reporte retenciones IIBB

---
+++ web/frontend/js/MetApp/app/view/Reportes/RepoRetenciones.js
@@ -0,0 +1,72 @@
+Ext.define('MetApp.view.Reportes.RepoRetenciones', {
+	extend: 'Ext.window.Window',
+	layout: 'fit',
+	width: 300,
+	autoShow: true,
+	modal: true,
+	alias: 'widget.RepoRetenciones',
+	layout: 'vbox',
+	title: 'Reporte Retenciones',
+	itemId: 'RepoRetenciones',
+	listeners: {
+		render: {
+			fn: function(el){
+				el.queryById('desde').focus('', 50);
+			}
+		}
+	},
+	
+	initComponent: function(){
+		var me = this;
+		Ext.applyIf(me, {
+			items: [
+				{
+					xtype: 'form',
+					itemId: 'fechaForm',
+					width: 295,
+					border: false,
+					layout: {
+						type: 'vbox',
+						align: 'center'	
+					},
+					defaults: {
+						margin: '2 2 2 2',	
+					},
+					fieldDefaults: {
+						allowBlank: false
+					},
+					items: [
+						{
+							xtype: 'datefield',
+							allowBlank: false,
+							itemId: 'desde',
+							format: 'd/m/Y',
+							submitFormat: 'd/m/Y',
+							name: 'desde',
+							fieldLabel: 'Desde',
+							labelWidth: 40
+						},
+						{
+							xtype: 'datefield',
+							format: 'd/m/Y',
+							submitFormat: 'd/m/Y',
+							allowBlank: false,
+							itemId: 'hasta',
+							name: 'hasta',
+							fieldLabel: 'Hasta',
+							labelWidth: 40
+						},
+						{
+							xtype: 'button',
+							itemId: 'printReport',
+							height: 50,
+							width: 50,
+							iconCls: 'reportes'
+						}
+					]
+				}		
+			]
+		});
+		this.callParent();
+	}
+});
