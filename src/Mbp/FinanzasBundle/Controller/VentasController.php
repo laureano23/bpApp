1c00e5e9ebb6518a2b9c88dc1c865c890e44fdef
laureano <laureano@server-linux.mbp>
2018-09-18 07:20:42 -0300

    Se agregan nuevos certificados para entorno de testing de facturacion. Se permiten eliminar cobranzas que no tengan cheques de terceros entregados a proveedores o depositados.

--- src/Mbp/FinanzasBundle/Controller/VentasController.php
+++ src/Mbp/FinanzasBundle/Controller/VentasController.php
@@ -482,7 +482,7 @@ class VentasController extends Controller
 			//si la cobranza tiene algun detalle asociado a un mov. bancario x ej deposito de cheque no se puede borrar
 			$detallesCobranza=$record->getCobranzaDetalleId();
 			foreach ($detallesCobranza as $det) {
-				if($det->getFormaPagoId()->getChequeTerceros()){ //
+				if($det->getEstado() != 0){ //Si el estado es distinto de cero es poque el cheque fue depositado o entregado a terceros y no puede borrarse la operacion
 					return $response->setContent(json_encode(array("success"=>false, "msg"=>"La cobranza tiene un movimiento bancario asociado")));
 				}					
 			}				
