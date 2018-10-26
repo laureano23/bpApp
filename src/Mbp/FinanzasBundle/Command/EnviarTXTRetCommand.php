c525beadcc86ad435019776eb36ac2f792ccb0b8
laureano <laureano@server-linux.mbp>
2018-10-26 11:55:27 -0300

    Cron job
    -Se agrega el reporte de retenciones al envio del TXT

--- src/Mbp/FinanzasBundle/Command/EnviarTXTRetCommand.php
+++ src/Mbp/FinanzasBundle/Command/EnviarTXTRetCommand.php
@@ -56,13 +56,72 @@ class EnviarTXTRetCommand extends ContainerAwareCommand
 
 		fclose($file);
 
+		//REPORTE
+		$reporteador = $this->getContainer()->get('reporteador');
+		$kernel = $this->getContainer()->get('kernel');
+
+		$jru = $reporteador->jru();		
+				
+		$ruta = $kernel->locateResource('@MbpProveedoresBundle/Reportes/Retenciones.jrxml');
+		$destino = $kernel->locateResource('@MbpProveedoresBundle/Resources/public/pdf/').'Retenciones.pdf';		
+		$param = $reporteador->getJava('java.util.HashMap');
+		$rutaLogo = $reporteador->getRutaLogo($kernel);
+		
+		$param->put('fechaDesde', $desde->format('d-m-Y'));
+		$param->put('fechaHasta', $hasta->format('d-m-Y'));
+
+		$desdeSql=$desde->format('Y/m/d');
+		$hastaSql=$hasta->format('Y/m/d');
+		$conn = $reporteador->getJdbc();
+
+		$sql = "select
+				DATE_FORMAT(op.fechaEmision, '%d/%m/%Y') AS fechaEmision,
+			    tipo.subTipoA,
+			    tipo.subTipoB,
+			    tipo.subTipoE,
+			    fc.sucursal,
+			    fc.numFc,
+			    op.id,
+			    prov.rsocial,
+			    prov.cuit,
+			    fc.neto,
+			    fc.totalFc,
+			    LPAD((truncate((tr.aplicado * pago.importe / baseImponible), 2)), 11 ,'0') AS retencion,
+			    baseImponible,
+			    tr.aplicado,
+			    pago.importe
+			from TransaccionOPFC tr
+				left join FacturaProveedor fc on fc.id = tr.facturaId
+			    left join TipoComprobante tipo on tipo.id=fc.tipoId
+				left join OrdenPago op on op.id = tr.ordenPagoId
+				inner join Proveedor prov on prov.id = op.proveedorId
+				inner join OrdenDePago_detallesPagos op_det on op_det.ordenPago_id = op.id
+				inner join Pago pago on pago.id = op_det.pago_id
+				left join FormasPagos fp on fp.id = pago.idFormaPago
+				inner join
+				(select SUM(case when f.neto > op.topeRetencionIIBB then tr.aplicado else 0 end) as baseImponible, op.id as opId
+					from TransaccionOPFC as tr
+					inner join FacturaProveedor f on f.id = tr.facturaId
+					inner join OrdenPago op on op.id = tr.ordenPagoId
+					where op.fechaEmision between '$desdeSql' and '$hastaSql'
+					group by op.id) as sub on sub.opId = op.id
+			where fp.retencionIIBB = true
+				and op.fechaEmision between '$desdeSql' and '$hastaSql'
+				and fc.neto > op.topeRetencionIIBB
+				group by tr.id";
+			
+		$jru->runPdfFromSql($ruta, $destino, $param, $sql, $conn->getConnection());
+		
+
         $mensaje = \Swift_Message::newInstance()
 				->setSubject("TXT Retenciones IIBB Metalurgica BP")
 				->setFrom('administracion@metalurgicabp.com.ar')
 				->setTo('lucilamariani@estudioplataroti.com.ar')
                 ->setBody("Este es un envío automático");
-        $adjunto1 = \Swift_Attachment::fromPath($basePath.$nombreArchivo);
-        $mensaje->attach($adjunto1);
+		$adjunto1 = \Swift_Attachment::fromPath($basePath.$nombreArchivo);
+		$adjunto2 = \Swift_Attachment::fromPath($destino);
+		$mensaje->attach($adjunto1);
+		$mensaje->attach($adjunto2);
 				
         $this->getContainer()->get('mailer')->send($mensaje);
     }
